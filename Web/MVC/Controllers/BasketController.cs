b33fee3366d43fc0e92f1510b0cae89388e82809
Dmytro <menshakovdmytro@gmail.com>
2022-07-18 16:32:19 +0300

    removed redundant code

--- Web/MVC/Controllers/BasketController.cs
+++ Web/MVC/Controllers/BasketController.cs
@@ -6,43 +6,33 @@ namespace MVC.Controllers;
 public class BasketController : Controller
 {
     private readonly IBasketService _basketService;
-    private readonly ICatalogService _catalogService;
     private readonly IIdentityParser<ApplicationUser> _appUserParser;
 
-    public BasketController(IBasketService basketService, ICatalogService catalogService, IIdentityParser<ApplicationUser> appUserParser)
+    public BasketController(IBasketService basketService, IIdentityParser<ApplicationUser> appUserParser)
     {
         _basketService = basketService;
-        _catalogService = catalogService;
         _appUserParser = appUserParser;
     }
 
     public async Task<IActionResult> Index()
     {
-        try
+        var user = GetCurrentUser();
+        var basket = await _basketService.GetBasket(user);
+        if (basket == null)
         {
-            var user = _appUserParser.Parse(HttpContext.User);
-            var vm = await _basketService.GetBasket(user);
-
-            return View(vm);
-        }
-        catch (Exception ex)
-        {
-            HandleException(ex);
+            return View("Error");
         }
 
-        return View();
+        return View(basket);
     }
 
     public async Task<IActionResult> UpdateQuantities(Dictionary<int, int> quantities)
     {
-        try
+        var user = GetCurrentUser();
+        var basket = await _basketService.SetQuantities(user, quantities);
+        if (basket == null)
         {
-            var user = _appUserParser.Parse(HttpContext.User);
-            var basket = await _basketService.SetQuantities(user, quantities);
-        }
-        catch (Exception ex)
-        {
-            HandleException(ex);
+            return View("Error");
         }
 
         return View("Index");
@@ -50,55 +40,38 @@ public class BasketController : Controller
 
     public async Task<IActionResult> AddToBasket(int id, string name, string price, string pictureUrl)
     {
-        try
+        var user = GetCurrentUser();
+        var unitPrice = 0M;
+        decimal.TryParse(price, out unitPrice);
+        var basketItem = new BasketItem()
         {
-            if (id != 0)
-            {
-                var user = _appUserParser.Parse(HttpContext.User);
-                var unitPrice = 0M;
-                decimal.TryParse(price, out unitPrice);
-                var basketItem = new BasketItem()
-                {
-                    Id = id,
-                    Name = name,
-                    Price = unitPrice,
-                    PictureUrl = pictureUrl,
-                    Quantity = 1
-                };
+            Id = id,
+            Name = name,
+            Price = unitPrice,
+            PictureUrl = pictureUrl,
+            Quantity = 1
+        };
 
-                await _basketService.AddToBasket(user, basketItem);
-            }
-
-            return RedirectToAction("Index", "Catalog");
-        }
-        catch (Exception ex)
-        {          
-            HandleException(ex);
+        var result = await _basketService.AddToBasket(user, basketItem);
+        if (result == null)
+        {
+            return View("Error");
         }
 
-        return RedirectToAction("Index", "Catalog", new { errorMsg = ViewBag.BasketInoperativeMsg });
+        return RedirectToAction("Index", "Catalog");
     }
 
     public async Task<IActionResult> RemoveFromBasket(int id)
     {
-        try
+        var user = GetCurrentUser();
+        var result = await _basketService.RemoveFromBasket(user, id);
+        if (result == null)
         {
-            if (id != 0)
-            {
-                var user = _appUserParser.Parse(HttpContext.User);
-                await _basketService.RemoveFromBasket(user, id);
-            }
-        }
-        catch (Exception ex)
-        {
-            HandleException(ex);
+            return View("Error");
         }
 
         return View("Index");
     }
 
-    private void HandleException(Exception ex)
-    {
-        ViewBag.BasketInoperativeMsg = $"Basket Service is inoperative {ex.GetType().Name} - {ex.Message}";
-    }
+    private ApplicationUser GetCurrentUser() => _appUserParser.Parse(HttpContext.User);
 }
