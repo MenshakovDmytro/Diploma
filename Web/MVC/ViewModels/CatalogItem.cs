635eaa075204cb18f9ed17841a901485420b8a5a
Dmytro <menshakovdmytro@gmail.com>
2022-07-21 12:15:22 +0300

    removed availableStock

--- Web/MVC/ViewModels/CatalogItem.cs
+++ Web/MVC/ViewModels/CatalogItem.cs
@@ -3,18 +3,10 @@
 public record CatalogItem
 {
     public int Id { get; set; }
-
     public string Name { get; set; } = null!;
-
     public string Description { get; set; } = null!;
-
     public decimal Price { get; set; }
-
     public string PictureUrl { get; set; } = null!;
-
     public CatalogMechanic CatalogType { get; set; } = null!;
-
     public CatalogCategory CatalogBrand { get; set; } = null!;
-
-    public int AvailableStock { get; set; }
 }
