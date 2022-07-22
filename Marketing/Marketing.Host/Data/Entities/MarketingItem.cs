95b419d992d73b67c79556272e179c8b58002078
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:36:36 +0300

    added MarketingItem

---
+++ Marketing/Marketing.Host/Data/Entities/MarketingItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Marketing.Host.Data.Entities;
+
+public class MarketingItem
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public string UserId { get; set; }
+    public string Username { get; set; }
+    public string Comment { get; set; }
+    public int Rating { get; set; }
+}
