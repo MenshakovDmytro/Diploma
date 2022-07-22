a24aa79cadca93b7f09b3cc0409550ed3dd0a295
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:49:57 +0300

    removed empty lines

--- Web/MVC/Models/Responses/CustomerBasketResponse.cs
+++ Web/MVC/Models/Responses/CustomerBasketResponse.cs
@@ -5,6 +5,5 @@ namespace MVC.Models.Responses;
 public class CustomerBasketResponse
 {
     public string? BuyerId { get; set; }
-
     public List<BasketItem> Items { get; set; } = new List<BasketItem>();
 }
