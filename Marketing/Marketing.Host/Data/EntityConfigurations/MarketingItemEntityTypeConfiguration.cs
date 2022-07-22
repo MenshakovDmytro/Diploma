1be2f773c35e3458bda1922f1594844248a493e5
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:37:49 +0300

    added MarketingItemEntityConfiguration

---
+++ Marketing/Marketing.Host/Data/EntityConfigurations/MarketingItemEntityTypeConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Marketing.Host.Data.Entities;
+
+namespace Marketing.Host.Data.EntityConfigurations;
+
+public class MarketingItemEntityTypeConfiguration
+    : IEntityTypeConfiguration<MarketingItem>
+{
+    public void Configure(EntityTypeBuilder<MarketingItem> builder)
+    {
+        builder.ToTable("Marketing");
+
+        builder.Property(mi => mi.Id)
+            .UseHiLo("marketing_hilo")
+            .IsRequired();
+
+        builder.Property(mi => mi.ProductId)
+            .IsRequired();
+
+        builder.Property(mi => mi.UserId)
+           .IsRequired()
+           .HasMaxLength(10);
+
+        builder.Property(mi => mi.Username)
+            .IsRequired(true)
+            .HasMaxLength(30);
+
+        builder.Property(mi => mi.Comment)
+            .IsRequired(true);
+
+        builder.Property(mi => mi.Rating)
+            .IsRequired(true);
+    }
+}
