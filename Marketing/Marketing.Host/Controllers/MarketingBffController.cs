85f440b65316db8fac6e4a0343f5153a6af87342
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:34:55 +0300

    added MarketingBffController

---
+++ Marketing/Marketing.Host/Controllers/MarketingBffController.cs
@@ -0,0 +1,34 @@
+using Marketing.Host.Models.Dtos;
+using Marketing.Host.Models.Requests;
+using Marketing.Host.Models.Responses;
+using Marketing.Host.Services.Interfaces;
+
+namespace Marketing.Host.Controllers;
+
+[ApiController]
+[Route(ComponentDefaults.DefaultRoute)]
+public class MarketingBffController : ControllerBase
+{
+    private readonly IMarketingItemService _marketingItemService;
+
+    public MarketingBffController(IMarketingItemService marketingItemService)
+    {
+        _marketingItemService = marketingItemService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(AddReviewResponse<int?>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> AddReview(AddReviewRequest request)
+    {
+        var result = await _marketingItemService.AddReview(request.ProductId, request.UserId, request.Username, request.Comment, request.Rating);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ItemsListResponse<MarketingItemDto>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetReviews(GetReviewsRequest request)
+    {
+        var result = await _marketingItemService.GetReviews(request.ProductId);
+        return Ok(result);
+    }
+}
