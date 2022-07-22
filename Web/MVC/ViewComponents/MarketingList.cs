22c2f11c0b65a2a85851f8da79858e22de76189f
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 18:07:13 +0300

    added MarketingList

---
+++ Web/MVC/ViewComponents/MarketingList.cs
@@ -0,0 +1,31 @@
+﻿using MVC.Services.Interfaces;
+using MVC.ViewModels.MarketingViewModels;
+
+namespace MVC.ViewComponents
+{
+    public class MarketingList : ViewComponent
+    {
+        private readonly IMarketingService _marketingService;
+
+        public MarketingList(IMarketingService marketingService)
+        {
+            _marketingService = marketingService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var vm = new MarketingViewModel
+            {
+                ProductId = id
+            };
+
+            var marketingItem = await _marketingService.GetReviews(id);
+            foreach (var item in marketingItem.Data)
+            {
+                vm.MarketingItems.Add(item);
+            }
+
+            return View(vm);
+        }
+    }
+}
