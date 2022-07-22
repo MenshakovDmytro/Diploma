e10339248d8a1b26c40e6cbcfa78dc39558e8dc9
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:44:44 +0300

    added IMarketingItemRepository

---
+++ Marketing/Marketing.Host/Repositories/Interfaces/IMarketingItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using Marketing.Host.Data;
+using Marketing.Host.Data.Entities;
+
+namespace Marketing.Host.Repositories.Interfaces;
+
+public interface IMarketingItemRepository
+{
+    Task<int?> AddAsync(int productId, string userId, string username, string comment, int rating);
+    Task<int?> RemoveAsync(int id);
+    Task<int?> UpdateAsync(int id, int productId, string userId, string username, string comment, int rating);
+    Task<ItemsList<MarketingItem>> GetItemsAsync(int productId);
+}
