658c4bf7dce73e6f9a61ff4e39bba2ff4812e255
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:39:02 +0300

    added migrations

---
+++ Marketing/Marketing.Host/Migrations/20220721215552_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marketing.Host.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "marketing_hilo",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "Marketing",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Username = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Comment = table.Column<string>(type: "text", nullable: false),
+                    Rating = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marketing", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marketing");
+
+            migrationBuilder.DropSequence(
+                name: "marketing_hilo");
+        }
+    }
+}
