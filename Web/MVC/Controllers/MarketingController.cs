ab3b03623a0aea888976dfeb48a4bc146501ae77
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:47:28 +0300

    added MarketingController

---
+++ Web/MVC/Controllers/MarketingController.cs
@@ -0,0 +1,30 @@
+﻿using MVC.Services.Interfaces;
+using MVC.ViewModels;
+
+namespace MVC.Controllers;
+
+public class MarketingController : Controller
+{
+    private readonly IMarketingService _marketingService;
+    private readonly IIdentityParser<ApplicationUser> _appUserParser;
+
+    public MarketingController(
+        IMarketingService marketingService,
+        IIdentityParser<ApplicationUser> appUserParser)
+    {
+        _marketingService = marketingService;
+        _appUserParser = appUserParser;
+    }
+
+    public async Task<IActionResult> AddReview(int productId, string comment, int rating)
+    {
+        var user = _appUserParser.Parse(HttpContext.User);
+        var result = await _marketingService.AddReview(productId, user, comment, rating);
+        if (result == null)
+        {
+            return View("Error");
+        }
+
+        return RedirectToAction("ProductPageInfo", "Catalog", new { id = productId });
+    }
+}
