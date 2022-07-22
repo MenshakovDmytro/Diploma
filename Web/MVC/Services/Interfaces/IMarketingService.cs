fe94086a4701bce4e23064d44d3163092d1672a9
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 18:06:06 +0300

    added RemoveReview method

---
+++ Web/MVC/Services/Interfaces/IMarketingService.cs
@@ -0,0 +1,12 @@
+﻿using MVC.Models.Response;
+using MVC.Models.Responses;
+using MVC.ViewModels;
+
+namespace MVC.Services.Interfaces;
+
+public interface IMarketingService
+{
+    Task<ItemsListResponse<MarketingItem>> GetReviews(int productId);
+    Task<AddReviewResponse<int?>> AddReview(int productId, ApplicationUser user, string comment, int rating);
+    Task<RemoveReviewResponse<int?>> RemoveReview(string userId);
+}
