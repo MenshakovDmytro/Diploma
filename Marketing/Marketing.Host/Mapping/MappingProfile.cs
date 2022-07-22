025dbbe128de9b90481f7206412ba320db9054f6
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:38:43 +0300

    added MappingProfile

---
+++ Marketing/Marketing.Host/Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using Marketing.Host.Data.Entities;
+using Marketing.Host.Models.Dtos;
+
+namespace Marketing.Host.Mapping;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<MarketingItem, MarketingItemDto>();
+    }
+}
