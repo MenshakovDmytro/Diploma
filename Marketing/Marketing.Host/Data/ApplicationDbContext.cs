43acf5909cb0f7411b03cd828ea795d04bc923e1
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:35:45 +0300

    added ApplicationDbContext

---
+++ Marketing/Marketing.Host/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Marketing.Host.Data.Entities;
+using Marketing.Host.Data.EntityConfigurations;
+
+namespace Marketing.Host.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<MarketingItem> MarketingItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new MarketingItemEntityTypeConfiguration());
+    }
+}
