b4df0a65cfc8581f0ccf92947c0a8875facac4a4
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:40:03 +0300

    added MarketingItemDto

---
+++ Marketing/Marketing.Host/Models/Dtos/MarketingItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Marketing.Host.Models.Dtos;
+
+public class MarketingItemDto
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public string UserId { get; set; }
+    public string Username { get; set; }
+    public string Comment { get; set; }
+    public int Rating { get; set; }
+}
