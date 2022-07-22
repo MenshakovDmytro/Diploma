30dfa91306660b5bde6e6a0e76c0b300240d3568
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:45:47 +0300

    added IMarketingItemService

---
+++ Marketing/Marketing.Host/Services/Interfaces/IMarketingItemService.cs
@@ -0,0 +1,10 @@
+﻿using Marketing.Host.Models.Dtos;
+using Marketing.Host.Models.Responses;
+
+namespace Marketing.Host.Services.Interfaces;
+
+public interface IMarketingItemService
+{
+    Task<AddReviewResponse<int?>> AddReview(int productId, string userId, string username, string comment, int rating);
+    Task<ItemsListResponse<MarketingItemDto>> GetReviews(int productId);
+}
