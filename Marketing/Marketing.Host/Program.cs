5c7f817214733e344a657877ab3c95d0f0397ffe
Dmytro <menshakovdmytro@gmail.com>
2022-07-22 15:42:34 +0300

    added Program

---
+++ Marketing/Marketing.Host/Program.cs
@@ -0,0 +1,127 @@
+using Infrastructure.Extensions;
+using Infrastructure.Filters;
+using Marketing.Host.Configurations;
+using Marketing.Host.Data;
+using Marketing.Host.Repositories;
+using Marketing.Host.Repositories.Interfaces;
+using Marketing.Host.Services;
+using Marketing.Host.Services.Interfaces;
+using Microsoft.OpenApi.Models;
+
+var configuration = GetConfiguration();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+})
+    .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "eShop-Marketing HTTP API",
+        Version = "v1",
+        Description = "Marketing Service HTTP API"
+    });
+
+    var authority = configuration["Authorization:Authority"];
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows()
+        {
+            Implicit = new OpenApiOAuthFlow()
+            {
+                AuthorizationUrl = new Uri($"{authority}/connect/authorize"),
+                TokenUrl = new Uri($"{authority}/connect/token"),
+                Scopes = new Dictionary<string, string>()
+                {
+                    { "mvc", "website" }
+                }
+            }
+        }
+    });
+
+    options.OperationFilter<AuthorizeCheckOperationFilter>();
+});
+
+builder.AddConfiguration();
+builder.Services.Configure<MarketingConfig>(configuration);
+
+builder.Services.AddAuthorization(configuration);
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddTransient<IMarketingItemRepository, MarketingItemRepository>();
+builder.Services.AddTransient<IMarketingItemService, MarketingItemService>();
+
+builder.Services.AddDbContextFactory<ApplicationDbContext>(opts => opts.UseNpgsql(configuration["ConnectionString"]));
+builder.Services.AddScoped<IDbContextWrapper<ApplicationDbContext>, DbContextWrapper<ApplicationDbContext>>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "CorsPolicy",
+        builder => builder
+            .SetIsOriginAllowed((host) => true)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+});
+
+var app = builder.Build();
+
+app.UseSwagger()
+    .UseSwaggerUI(setup =>
+    {
+        setup.SwaggerEndpoint($"{configuration["PathBase"]}/swagger/v1/swagger.json", "Marketing.API V1");
+        setup.OAuthClientId("marketingswaggerui");
+        setup.OAuthAppName("Marketing Swagger UI");
+    });
+
+app.UseRouting();
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapControllers();
+});
+
+CreateDbIfNotExists(app);
+app.Run();
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    return builder.Build();
+}
+
+void CreateDbIfNotExists(IHost host)
+{
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<ApplicationDbContext>();
+
+            DbInitializer.Initialize(context).Wait();
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred creating the DB.");
+        }
+    }
+}
