6e23c5a9bcd44ea5068407a2ba7b2953c02488da
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:43:54 +0300

    added EditItemViewModel

---
+++ Web/MVC/ViewModels/ManagerViewModels/EditItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.ViewModels.ManagerViewModels;
+
+public class EditItemViewModel
+{
+    public CatalogItem CatalogItem { get; set; }
+    public IEnumerable<SelectListItem> Categories { get; set; }
+    public IEnumerable<SelectListItem> Mechanics { get; set; }
+    public int CategoryId { get; set; }
+    public int MechanicId { get; set; }
+}
