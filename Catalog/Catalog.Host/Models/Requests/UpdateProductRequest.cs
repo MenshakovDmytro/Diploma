f008dbc047d0c320b8fcd341a74cf3026e445c0e
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:11:34 +0300

    removed pictureUrl property

--- Catalog/Catalog.Host/Models/Requests/UpdateProductRequest.cs
+++ Catalog/Catalog.Host/Models/Requests/UpdateProductRequest.cs
@@ -6,7 +6,6 @@ public class UpdateProductRequest
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public decimal Price { get; set; }
-    public string PictureFileName { get; set; } = null!;
     public int CatalogMechanicId { get; set; }
     public int CatalogCategoryId { get; set; }
 }
