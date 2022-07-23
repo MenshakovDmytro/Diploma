c8158cb063155ef22a47ae69534e06902510c76e
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:19:53 +0300

    removed pictureFileName from methods

--- Catalog/Catalog.Host/Services/Interfaces/ICatalogItemService.cs
+++ Catalog/Catalog.Host/Services/Interfaces/ICatalogItemService.cs
@@ -5,9 +5,9 @@ namespace Catalog.Host.Services.Interfaces;
 
 public interface ICatalogItemService
 {
-    Task<AddItemResponse<int?>> AddAsync(string name, string description, decimal price, int catalogCategoryId, int catalogMechanicId, string pictureFileName);
+    Task<AddItemResponse<int?>> AddAsync(string name, string description, decimal price, int catalogCategoryId, int catalogMechanicId);
     Task<RemoveItemResponse<int?>> RemoveAsync(int id);
-    Task<UpdateCategoryResponse<int?>> UpdateAsync(int id, string name, string description, decimal price, int catalogCategoryId, int catalogMechanicId, string pictureFileName);
+    Task<UpdateCategoryResponse<int?>> UpdateAsync(int id, string name, string description, decimal price, int catalogCategoryId, int catalogMechanicId);
     Task<ItemsListResponse<CatalogItemDto>> GetItemsAsync();
     Task<GetItemResponse<CatalogItemDto>> GetItemAsync(int id);
 }
