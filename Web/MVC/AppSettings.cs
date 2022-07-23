a10c7dd0e1fd99be2c5a8ea199446d794ed101b0
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:24:18 +0300

    added CatalogItemUrl, CatalogCategoryUrl, CatalogMechanicUrl

--- Web/MVC/AppSettings.cs
+++ Web/MVC/AppSettings.cs
@@ -5,6 +5,9 @@ public class AppSettings
     public string CatalogUrl { get; set; }
     public string BasketUrl { get; set; }
     public string MarketingUrl { get; set; }
+    public string CatalogItemUrl { get; set; }
+    public string CatalogCategoryUrl { get; set; }
+    public string CatalogMechanicUrl { get; set; }
     public int SessionCookieLifetimeMinutes { get; set; }    
     public string CallBackUrl { get; set; }
     public string IdentityUrl { get; set; }
