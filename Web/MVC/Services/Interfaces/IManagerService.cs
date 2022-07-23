5d0ab40c932d8eb0ca58d6442cb652e1b8a7ae2f
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:51:04 +0300

    added IManagerService

---
+++ Web/MVC/Services/Interfaces/IManagerService.cs
@@ -0,0 +1,16 @@
+﻿using MVC.Models.Response;
+using MVC.Models.Responses;
+using MVC.ViewModels;
+
+namespace MVC.Services.Interfaces;
+
+public interface IManagerService
+{
+    Task<ItemsListResponse<CatalogItem>> GetCatalogItems();
+    Task<GetItemResponse<CatalogItem>> GetCatalogItem(int id);
+    Task<CatalogItem?> AddItem(string name, string description, decimal price, int catalogCategoryId, int catalogMechanicId);
+    Task<CatalogItem?> RemoveItem(int id);
+    Task<CatalogItem?> UpdateItem(int id, string name, string description, decimal price, int catalogCategoryId, int catalogMechanicId);
+    Task<IEnumerable<SelectListItem>> GetCategoriesSelectedList();
+    Task<IEnumerable<SelectListItem>> GetMechanicsSelectedList();
+}
