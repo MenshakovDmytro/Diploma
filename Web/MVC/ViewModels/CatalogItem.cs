57c501510b5a32bf6f4de9622c782a4ac54c9820
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:55:12 +0300

    added PictureUrl

--- Web/MVC/ViewModels/CatalogItem.cs
+++ Web/MVC/ViewModels/CatalogItem.cs
@@ -6,6 +6,7 @@ public record CatalogItem
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
     public decimal Price { get; set; }
+    public string PictureUrl { get; set; } = null!;
     public CatalogMechanic CatalogMechanic { get; set; } = null!;
     public CatalogCategory CatalogCategory { get; set; } = null!;
 }
