b204e005d9f82e15cc8a43252f5f22be9184f963
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:09:49 +0300

    removed logger and added GetCatalogItem method

--- Catalog/Catalog.Host/Controllers/CatalogItemController.cs
+++ Catalog/Catalog.Host/Controllers/CatalogItemController.cs
@@ -3,31 +3,43 @@ using Infrastructure.Identity;
 using Catalog.Host.Models.Requests;
 using Catalog.Host.Models.Response;
 using Catalog.Host.Services.Interfaces;
+using Catalog.Host.Models.Dtos;
 
 namespace Catalog.Host.Controllers;
 
 [ApiController]
 [Authorize(Policy = AuthPolicy.AllowClientPolicy)]
-[Scope("catalog.catalogitem")]
 [Route(ComponentDefaults.DefaultRoute)]
 public class CatalogItemController : ControllerBase
 {
-    private readonly ILogger<CatalogItemController> _logger;
     private readonly ICatalogItemService _catalogItemService;
 
-    public CatalogItemController(
-        ILogger<CatalogItemController> logger,
-        ICatalogItemService catalogItemService)
+    public CatalogItemController(ICatalogItemService catalogItemService)
     {
-        _logger = logger;
         _catalogItemService = catalogItemService;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(ItemsListResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetCatalogItems()
+    {
+        var result = await _catalogItemService.GetItemsAsync();
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(GetItemResponse<CatalogItemDto>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetCatalogItem(GetItemRequest request)
+    {
+        var result = await _catalogItemService.GetItemAsync(request.Id);
+        return Ok(result);
     }
 
     [HttpPost]
     [ProducesResponseType(typeof(AddItemResponse<int?>), (int)HttpStatusCode.OK)]
     public async Task<IActionResult> Add(CreateProductRequest request)
     {
-        var result = await _catalogItemService.AddAsync(request.Name, request.Description, request.Price, request.CatalogCategoryId, request.CatalogMechanicId, request.PictureFileName);
+        var result = await _catalogItemService.AddAsync(request.Name, request.Description, request.Price, request.CatalogCategoryId, request.CatalogMechanicId);
         return Ok(result);
     }
 
@@ -43,7 +55,7 @@ public class CatalogItemController : ControllerBase
     [ProducesResponseType(typeof(UpdateItemResponse<int?>), (int)HttpStatusCode.OK)]
     public async Task<IActionResult> Update(UpdateProductRequest request)
     {
-        var result = await _catalogItemService.UpdateAsync(request.Id, request.Name, request.Description, request.Price, request.CatalogCategoryId, request.CatalogMechanicId, request.PictureFileName);
+        var result = await _catalogItemService.UpdateAsync(request.Id, request.Name, request.Description, request.Price, request.CatalogCategoryId, request.CatalogMechanicId);
         return Ok(result);
     }
 }
