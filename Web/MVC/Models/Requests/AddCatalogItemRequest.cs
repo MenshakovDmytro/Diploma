c9d31f3edea43344b431d3c88b3bf0d2b32cc06e
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:27:39 +0300

    added AddCatalogItemRequest

---
+++ Web/MVC/Models/Requests/AddCatalogItemRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.Models.Requests;
+
+public class AddCatalogItemRequest
+{
+    public string Name { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int CatalogMechanicId { get; set; }
+    public int CatalogCategoryId { get; set; }
+}
