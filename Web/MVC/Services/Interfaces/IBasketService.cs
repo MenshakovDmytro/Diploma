6428907cefb19f7cfa4298c70b64b73b6c1af77b
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:39:23 +0300

    changed AddItemResponse to AddItemResponse<bool>

--- Web/MVC/Services/Interfaces/IBasketService.cs
+++ Web/MVC/Services/Interfaces/IBasketService.cs
@@ -5,7 +5,7 @@ namespace MVC.Services.Interfaces;
 
 public interface IBasketService
 {
-    Task<AddItemResponse> AddToBasket(ApplicationUser user, BasketItem basketItem);
+    Task<AddItemResponse<bool>> AddToBasket(ApplicationUser user, BasketItem basketItem);
     Task<RemoveFromBasketResponse> RemoveFromBasket(ApplicationUser user, int itemId);
     Task<CustomerBasket> GetBasket(ApplicationUser user);
     Task<DeleteBasketResponse> DeleteBasket(ApplicationUser user);
