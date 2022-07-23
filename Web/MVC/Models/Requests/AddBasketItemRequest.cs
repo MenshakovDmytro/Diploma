d397adc4fdb4379d34ad25d67bf90064d2e90362
Dmytro <menshakovdmytro@gmail.com>
2022-07-23 23:26:38 +0300

    added AddBasketItemRequest

---
+++ Web/MVC/Models/Requests/AddBasketItemRequest.cs
@@ -0,0 +1,10 @@
+﻿using MVC.ViewModels;
+
+namespace MVC.Models.Requests
+{
+    public class AddBasketItemRequest
+    {
+        public string Id { get; set; }
+        public BasketItem BasketItem { get; set; }
+    }
+}
