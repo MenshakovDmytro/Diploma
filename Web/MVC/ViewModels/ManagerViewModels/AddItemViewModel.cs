1c2b0837a81ea2b1a6596974648694e24c46984f
Dmytro <menshakovdmytro@gmail.com>
2022-07-24 21:48:23 +0300

    added AddItemViewModel

---
+++ Web/MVC/ViewModels/ManagerViewModels/AddItemViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC.ViewModels.ManagerViewModels;
+
+public class AddItemViewModel
+{
+    public IEnumerable<SelectListItem> Categories { get; set; } = null!;
+    public IEnumerable<SelectListItem> Mechanics { get; set; } = null!;
+    public int CategoryId { get; set; }
+    public int MechanicId { get; set; }
+}
