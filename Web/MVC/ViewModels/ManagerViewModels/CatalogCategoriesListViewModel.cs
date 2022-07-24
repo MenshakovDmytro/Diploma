be6b133fba1933fcec2bc775a1f4131c556f333a
Dmytro <menshakovdmytro@gmail.com>
2022-07-24 21:50:22 +0300

    added CatalogCategoriesListViewModel

---
+++ Web/MVC/ViewModels/ManagerViewModels/CatalogCategoriesListViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MVC.ViewModels.ManagerViewModels;
+
+public class CatalogCategoriesListViewModel
+{
+    public List<CatalogCategory> CatalogCategories { get; set; } = new List<CatalogCategory>();
+}
